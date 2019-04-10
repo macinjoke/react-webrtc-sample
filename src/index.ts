927e87cfd2ab63210bb702c8c42279a3666ee327
macinjoke <shunji.makino@gmail.com>
2019-04-10 22:31:09 +0900

    webpack

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log('hello')
+function f1() {
+  console.log('f1')
+}
+const f3 = () => {
+  console.log('f3')
+}
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.ts', '.tsx', '.js'
+    ]
+  },
+  devServer: {
+    contentBase: 'dist'
+  }
+}
