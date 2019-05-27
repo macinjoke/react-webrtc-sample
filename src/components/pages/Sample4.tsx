63a6c83eab20e66b46b616f2bd99406710b46638
macinjoke <shunji.makino@gmail.com>
2019-05-27 13:49:58 +0900

    fix Sample4 ( remove waste states )

--- src/components/pages/Sample4.tsx
+++ src/components/pages/Sample4.tsx
@@ -4,48 +4,51 @@ import io from 'socket.io-client'
 interface Props {}
 interface State {
   isInitiator: boolean
-  socket?: SocketIOClient.Socket
 }
 
 /**
  * communicate to a signaling server
  */
 class Sample4 extends React.Component<Props, State> {
+  private socket?: SocketIOClient.Socket
   public constructor(props: Props) {
     super(props)
     this.state = { isInitiator: false }
   }
 
   public componentDidMount(): void {
-    const socket = io.connect('http://localhost:8000')
+    this.socket = io.connect('http://localhost:8000')
     const room = prompt('Enter room name:')
     if (room !== '') {
       console.log('Message from client: Asking to join room ' + room)
-      socket.emit('create or join', room)
+      this.socket.emit('create or join', room)
     }
 
-    socket.on('created', (room: string, clientId: string) => {
+    this.socket.on('created', (room: string, clientId: string) => {
       console.log(room, clientId)
       this.setState({ isInitiator: true })
     })
 
-    socket.on('full', (room: string) => {
+    this.socket.on('full', (room: string) => {
       console.log('Message from client: Room ' + room + ' is full :^(')
     })
 
-    socket.on('ipaddr', (ipaddr: string) => {
+    this.socket.on('ipaddr', (ipaddr: string) => {
       console.log('Message from client: Server IP address is ' + ipaddr)
     })
 
-    socket.on('joined', (room: string, clientId: string) => {
+    this.socket.on('joined', (room: string, clientId: string) => {
       console.log(room, clientId)
       this.setState({ isInitiator: false })
     })
 
-    socket.on('log', (text: string) => {
+    this.socket.on('log', (text: string) => {
       console.log(text)
     })
-    this.setState({ socket })
+  }
+
+  public async componentWillUnmount() {
+    if (this.socket) this.socket.close()
   }
 
   public render() {
